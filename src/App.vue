27625378a5841c2990fa6675f20fd4102c0648e0
SaidahmadRasulov <said,rasulov.2018@list.ru>
2023-11-27 16:19:03 +0500

    initial project with important tools

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import customHeader from './components/customHeader.vue'
+import products from './data/products.json'
+</script>
+
+<template>
+  <customHeader />
+  <RouterView />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: products.data
+    }
+  },
+  components: {
+    RouterView
+  },
+  mounted() {
+    console.log(this.products)
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center">
+        <h1>Not Found</h1>
+        <h2>404</h2>
+    </div>
+</template>
