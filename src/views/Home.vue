5a02407428b39b3962e03fbb6cbf1ea002275e6e
SaidahmadRasulov <said,rasulov.2018@list.ru>
2023-11-28 18:46:10 +0500

    docs: Making adverb section in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="bg-[#F0F3F7] py-[3rem]">
     <Categories :selectedValue=selectedValue :products="products" :categories="categories" />
+    <Products :selectedValue=selectedValue :products="products" />
   </div>
 </template>
 <script>
 import Categories from '../components/Categories.vue'
+import Products from '../components/Products.vue';
 
 export default {
-  components: { Categories },
+  components: { Categories, Products },
   props: {
     products: {
         type: Array,
